2fa63ef52c24fce4ee929c1036c9ee8023bef756
REPVL <ralph_viceral@dlsu.edu.ph>
2024-03-23 13:00:42 +0800

    updated download function

--- public/scripts.js
+++ public/scripts.js
@@ -15,23 +15,25 @@ $(document).ready(() => {
 
     $('#ccButton').on('click', () => {
         try {
-            if ($('#imagePreview').attr('src') != null) {
+            if ($('#imagePreview').attr('src') !== '') {
                 $('<a>', {
                     href: $('#imagePreview').attr('src'), download: 'color-correct-image.jpg',
                 }).appendTo('body').get(0).click().remove()
             }
         } catch (err) {
+            console.log('image has not rendered or does not exist.');
         }
     })
 
     $('#edButton').on('click', () => {
         try {
-            if ($('#imagePreview').attr('src') != null) {
+            if ($('#imagePreview').attr('src') !== '') {
                 $('<a>', {
                     href: $('#imagePreview').attr('src'), download: 'edge-detected-image.jpg',
                 }).appendTo('body').get(0).click().remove()
             }
         } catch (err) {
+            console.log('image has not rendered or does not exist.');
         }
     })
 })
