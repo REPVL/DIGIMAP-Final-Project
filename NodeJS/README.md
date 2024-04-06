ad55c4b15a05d817768a26d0da7a3d684feced85
REPVL <ralph_viceral@dlsu.edu.ph>
2024-04-06 10:55:43 +0800

    clean-up

--- NodeJS/README.md
+++
@@ -1,4 +0,0 @@
-# Final Project 2 (Color Correction, Edge Detection)
-
-  # Instructions
-    # npm install express
--- NodeJS/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const controller = require('../controllers/controller.js')
-const app = express()
-
-app.get('/favicon.ico', controller.showIcon)
-app.get('/', controller.showIndex)
-
-module.exports = app
